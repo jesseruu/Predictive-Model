655839fff978eed5d1072235fe3ef1bd549cafaa
Jesser Lemus <ijesser@outlook.com>
2021-04-27 19:36:13 -0500

    Read new clean dataset and delete useless column

--- prediction_air.py
+++ prediction_air.py
@@ -1,3 +1,14 @@
 import preprocesamiento as pre
 
-print(pre.air_data.head(10))
+def comprueba_data():
+    if not pre.os.path.exists('train'):
+        pre.main()
+
+def data():
+    ica_quality = pre.pd.read_csv('train/minambiente_train_data.csv')
+    ica_quality.drop(['Unnamed: 0'], axis = 1, inplace = True)
+    print(ica_quality.head(10))
+
+if __name__ == '__main__':
+    comprueba_data()
+    data()
