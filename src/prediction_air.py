9542659d05de3a0310c222565c528b5958733437
Jesser Lemus <ijesser@outlook.com>
2021-05-27 23:43:06 -0500

    Add prediction model

--- src/prediction_air.py
+++ src/prediction_air.py
@@ -1,13 +1,35 @@
-import preprocesamiento as pre
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+import numpy as np
 
-def comprueba_data():
-    if not pre.os.path.exists('train'):
-        pre.main()
+ica_quality = pd.read_csv('train/minambiente_train_data.csv')
 
-def data():
-    ica_quality = pre.pd.read_csv('train/minambiente_train_data.csv')
-    ica_quality.drop(['Unnamed: 0'], axis = 1, inplace = True)
+X = ica_quality['FECHA'].values
+op = input("Seleccione una opcion: ").upper()
+y = ica_quality[op].values
 
-if __name__ == '__main__':
-    comprueba_data()
-    data()
+X = np.array(X).reshape(-1,1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+clf = RandomForestRegressor(n_estimators=200)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+# Rendimiento del modelo
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+print('Precision:', clf.score(X_train, y_train))
+
+'''plt.scatter(X_test['FECHA:MONTH'], y_test, color = "blue")
+plt.scatter(X_test['FECHA:MONTH'], y_pred, color = "green")
+plt.title("Modelo de prediccion")
+plt.xlabel("Fechas")
+plt.ylabel(op)
+plt.xticks(rotation = 45)
+plt.show()'''
