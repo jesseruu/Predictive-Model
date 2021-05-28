8d3060313925d05dbc51ca05c3b43f1cef99bc32
Jesser Lemus <ijesser@outlook.com>
2021-05-28 00:00:27 -0500

    Add helpful comments

--- src/prediction_air.py
+++ src/prediction_air.py
@@ -4,32 +4,30 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics
 import numpy as np
 
+# Importamos el dataset limpio
 ica_quality = pd.read_csv('train/minambiente_train_data.csv')
 
+# Definimos las variables de X, y dependiendo de la particula 
 X = ica_quality['FECHA'].values
 op = input("Seleccione una opcion: ").upper()
 y = ica_quality[op].values
 
 X = np.array(X).reshape(-1,1)
 
+# Dividimos el dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
+# Se entrena el modelo
 clf = RandomForestRegressor(n_estimators=200)
 clf.fit(X_train, y_train)
 
+# Una vez entrenado, se establece el modelo predictivo
 y_pred = clf.predict(X_test)
 
 # Rendimiento del modelo
-print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
-print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
-print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+print('Error Medio Absoluto:', metrics.mean_absolute_error(y_test, y_pred))
+print('Error medio cuadratico:', metrics.mean_squared_error(y_test, y_pred))
+print('Raiz error medio cuadrático:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
 
+# Precision
 print('Precision:', clf.score(X_train, y_train))
-
-'''plt.scatter(X_test['FECHA:MONTH'], y_test, color = "blue")
-plt.scatter(X_test['FECHA:MONTH'], y_pred, color = "green")
-plt.title("Modelo de prediccion")
-plt.xlabel("Fechas")
-plt.ylabel(op)
-plt.xticks(rotation = 45)
-plt.show()'''
